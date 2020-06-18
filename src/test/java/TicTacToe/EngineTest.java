93e3e2e2dcf2d92bd5dc5fd7852d6c6c4c8abee1
Kevin Scardina <kscardina@gmail.com>
2020-06-17 19:01:43 -0600

    test 2

--- src/test/java/TicTacToe/EngineTest.java
+++ src/test/java/TicTacToe/EngineTest.java
@@ -7,4 +7,9 @@ public class EngineTest {
     @Test public void testTwoTokens() {
         assertTrue("should have 2 tokens", Engine.Token.values().length == 2);
     }
+
+    @Test public void test3x3board() {
+        Engine engine = Engine();
+        assertTrue("should have a 3x3 board", Engine.board().length == 3*3);
+    }
 }
