0324f7efa17c7bf62bc80c562e49b4c2f96c573e
Kevin Scardina <kscardina@gmail.com>
2020-06-17 19:15:14 -0600

    impl 2

--- src/main/java/TicTacToe/Engine.java
+++ src/main/java/TicTacToe/Engine.java
@@ -4,12 +4,4 @@ public class Engine {
     public enum Token {
         X, O
     }
-
-    public Token[] board() { return _board.clone(); }
-
-    public Engine() {
-        _board = new Token[9];
-    }
-    
-    private Token[] _board;
 }
--- src/test/java/TicTacToe/EngineTest.java
+++ src/test/java/TicTacToe/EngineTest.java
@@ -7,9 +7,4 @@ public class EngineTest {
     @Test public void testTwoTokens() {
         assertTrue("should have 2 tokens", Engine.Token.values().length == 2);
     }
-
-    @Test public void test3x3board() {
-        Engine engine = new Engine();
-        assertTrue("should have a 3x3 board", engine.board().length == 3*3);
-    }
 }
