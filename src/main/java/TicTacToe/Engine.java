944377d551ea6e1b39f03dfd93d2c25ebadcbbec
Kevin Scardina <kscardina@gmail.com>
2020-06-17 19:48:44 -0600

    impl first token x

--- src/main/java/TicTacToe/Engine.java
+++ src/main/java/TicTacToe/Engine.java
@@ -6,10 +6,13 @@ public class Engine {
     }
 
     public Token[] board() { return _board.clone(); }
+    public Token turn() { return _turn; }
 
     public Engine() {
         _board = new Token[9];
+        _turn = Token.X;
     }
     
     private Token[] _board;
+    private Token _turn;
 }
