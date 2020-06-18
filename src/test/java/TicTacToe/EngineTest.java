1707c6c182210b8fa4d72e8e65053059f4dc10f9
Kevin Scardina <kscardina@gmail.com>
2020-06-17 19:33:43 -0600

    test 3

--- src/test/java/TicTacToe/EngineTest.java
+++ src/test/java/TicTacToe/EngineTest.java
@@ -1,6 +1,9 @@
 package TicTacToe;
 
 import org.junit.Test;
+
+import TicTacToe.Engine.Token;
+
 import static org.junit.Assert.*;
 
 public class EngineTest {
@@ -11,5 +14,12 @@ public class EngineTest {
     @Test public void test3x3board() {
         Engine engine = new Engine();
         assertTrue("should have a 3x3 board", engine.board().length == 3*3);
+    }
+
+    @Test public void testBoardEmptyOnStart() {
+        Engine engine = new Engine();
+        for (Token spot : engine.board()) {
+            assertTrue("board should be empty on start", spot == null);
+        }
     }
 }
