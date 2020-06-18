a26b5ac89a4dd8aa6942c8858fb6f104762bf4c8
Kevin Scardina <kscardina@gmail.com>
2020-06-17 19:36:51 -0600

    refactor 3

--- src/test/java/TicTacToe/EngineTest.java
+++ src/test/java/TicTacToe/EngineTest.java
@@ -6,18 +6,24 @@ import TicTacToe.Engine.Token;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+
 public class EngineTest {
+    private Engine engine;
+
+    @Before public void setupEngine() {
+        engine = new Engine();
+    }
+
     @Test public void testTwoTokens() {
         assertTrue("should have 2 tokens", Engine.Token.values().length == 2);
     }
 
     @Test public void test3x3board() {
-        Engine engine = new Engine();
         assertTrue("should have a 3x3 board", engine.board().length == 3*3);
     }
 
     @Test public void testBoardEmptyOnStart() {
-        Engine engine = new Engine();
         for (Token spot : engine.board()) {
             assertTrue("board should be empty on start", spot == null);
         }
