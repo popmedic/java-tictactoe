9745375800debffb15e878aa54a0d33abafb7725
Kevin Scardina <kscardina@gmail.com>
2020-06-18 19:05:21 -0600

    clean up

--- src/main/java/TicTacToe/Engine.java
+++ src/main/java/TicTacToe/Engine.java
@@ -13,27 +13,17 @@ public class Engine {
     public Token turn() { return _turn; }
 
     public Status play(int spot) {
-        if (isSpotOOB(spot)) { return Status.OOB; }
-        if (isSpotEmpty(spot)) { return Status.SAME_SPOT; }
+        if (isSpotOOB(spot)) return Status.OOB;
+        if (isSpotEmpty(spot)) return Status.SAME_SPOT;
+        
         _board[spot] = turn();
 
-        for (Integer[] row : WIN_TABLE) {
-            if (_board[row[0]] != null && 
-                _board[row[0]] == _board[row[1]] && 
-                _board[row[1]] == _board[row[2]]) {
-                
-                return Status.WIN;
-            }
-        }
+        if (isWin()) return Status.WIN;
+        if (isCatGame()) return Status.CAT_GAME;
+        
+        _turn = toggle(turn());
 
-        for (Token token : _board) {
-            if (token == null) {
-                _turn = toggle(_turn);
-                return Status.SUCCESS;
-            }
-        }
-
-        return Status.CAT_GAME;
+        return Status.SUCCESS;
     }
 
     public Engine() {
@@ -42,11 +32,35 @@ public class Engine {
     }
     
     private Token[] _board;
+    
     private Token _turn;
+    
     private Token toggle(Token token) { return _turn == Token.X ? Token.O : Token.X; }
+    
     private boolean isSpotEmpty(int spot) { return _board[spot] != null; }
+    
     private boolean isSpotOOB(int spot) { return spot < 0 || spot > 8; }
-    private final Integer[][] WIN_TABLE = {
+    
+    private boolean isWin() {
+        for (int[] row : WIN_TABLE) {
+            if (_board[row[0]] != null &&
+                _board[row[0]] == _board[row[1]] && 
+                _board[row[1]] == _board[row[2]]) {
+                
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isCatGame() {
+        for (Token token : _board) {
+            if (token == null) return false;
+        }
+        return true;
+    }
+
+    private final int[][] WIN_TABLE = {
         {0,1,2}, 
         {3,4,5}, 
         {6,7,8},
